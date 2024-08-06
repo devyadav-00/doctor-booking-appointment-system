2641504ecb27a00582aaa6de839198ef7bb47a5d
devyadav-00 <dev428845@gmail.com>
2024-08-06 23:50:34 +0530

    backend url added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server:{
     proxy: {
-      '/api/v1': 'http://localhost:5432'
+      '/api/v1': 'https://doctor-booking-appointment-system-backend.onrender.com'
     }
   },
   plugins: [react()],
